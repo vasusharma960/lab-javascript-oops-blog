074a3a74ba20070cdb5dc1ab3cc2dd4346d26f16
vasusharma960 <vasusharma.2058@gmail.com>
2022-04-22 01:34:51 +0530

    Initial Commit

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
 /* Fill your code*/
+class Blog{
+  constructor(title, detail){
+    this.title = title;
+    this.detail = detail;
+  }
+
+  addTitle(){
+    var title_card = document.createElement('h1');
+    title_card.setAttribute("id","blog-title");
+    document.getElementById('card-text').appendChild(title_card);
+    title_card.innerHTML += this.title;
+  }
+
+  addDescription(){
+    var description_card = document.createElement('p');
+    description_card.setAttribute("id", "blog-description");
+    document.getElementById('card-text').appendChild(description_card);
+    description_card.innerHTML += this.detail;
+  }
+}
+
+function addPost(){
+  document.getElementById("popupContact").style.display = "block";
+
+  document.getElementById("close").addEventListener("click", function(){
+    document.getElementById("popupContact").style.display = "none";
+  });
+}
+
+function post(){
+  let title = document.getElementById("title").value;
+  let description = document.getElementById("detail").value;
+
+  let image = document.createElement("img");
+  image.src = "assets/javascript.png";
+  document.getElementById('card-text').appendChild(image);
+
+  var obj = new Blog(title, description);
+  obj.addTitle();
+  obj.addDescription();
+
+  document.getElementById("popupContact").style.display = "none";
+}
